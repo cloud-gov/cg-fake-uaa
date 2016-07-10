18108f67757107ba80d11caf54f2a1050fdc9a9d
Atul Varma <varmaa@gmail.com>
2016-07-10 01:59:59 -0400

    Factor out server.go.

--- fake-cloud.gov.go
+++ fake-cloud.gov.go
@@ -4,37 +4,7 @@ import (
 	"fmt"
 	"flag"
 	"net/http"
-	"net/url"
 )
-
-type ServerConfig struct {
-	CallbackUrl *url.URL
-}
-
-func NewHandler(config *ServerConfig) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		baseHandler(config, w, r)
-	}
-}
-
-func baseHandler(config *ServerConfig, w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == Urls.Reverse("authorize") && r.Method == "GET" {
-		Authorize(config, w, r)
-	} else if r.URL.Path == Urls.Reverse("token") && r.Method == "POST" {
-		ExchangeCodeForAccessToken(w, r)
-	} else if r.URL.Path == Urls.Reverse("svgLogo") {
-		data, err := Asset("data/fake-cloud.gov.svg")
-		if err != nil {
-			panic("Couldn't find fake-cloud.gov.svg!")
-		}
-		w.Header().Set("Content-Type", "image/svg+xml")
-		fmt.Fprintf(w, "%s", data)
-	} else {
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(404)
-		fmt.Fprintf(w, "Not Found")
-	}
-}
 
 func main() {
 	var callbackUrl string
