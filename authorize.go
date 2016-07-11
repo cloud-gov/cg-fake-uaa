3b719cf230a7ea4f80a95eb39c4fc2d4d5e92774
Atul Varma <varmaa@gmail.com>
2016-07-11 07:05:19 -0400

    Add warnings to login page.

--- authorize.go
+++ authorize.go
@@ -7,6 +7,10 @@ import (
 	"net/url"
 )
 
+const CLIENT_ID_WARNING = `The <code>client_id</code> querystring parameter is missing.`
+const RESPONSE_TYPE_WARNING = `The <code>response_type</code> querystring parameter is expected to be <code>code</code>.`
+const STATE_WARNING = `The <code>state</code> querystring parameter is missing. See <a href="http://www.twobotechnologies.com/blog/2014/02/importance-of-state-in-oauth2.html">The Importance of the state parameter in OAuth2</a> for more details.`
+
 var taglines = [...]string{
 	`Welcome to your fake <abbr title="User Account and Authentication">UAA</abbr> provider!`,
 	`The convenience of zero-factor authentication is here.`,
@@ -16,6 +20,7 @@ var taglines = [...]string{
 type loginPageContext struct {
 	Tagline   template.HTML
 	QueryArgs map[string]string
+	Warnings  []template.HTML
 }
 
 func getRandomTagline() string {
@@ -43,18 +48,33 @@ func redirectToCallback(w http.ResponseWriter, u url.URL, code string, state str
 }
 
 func Authorize(config *ServerConfig, w http.ResponseWriter, r *http.Request) {
+	var warnings []template.HTML
+
 	rq := r.URL.Query()
 	email := rq.Get("email")
-	// TODO: Ensure 'client_id' is in GET params.
-	// TODO: Ensure 'state' is in GET params.
-	// TODO: Ensure 'response_type' is 'code'.
-	if len(email) == 0 {
+
+	addWarning := func(warning string) {
+		warnings = append(warnings, template.HTML(warning))
+	}
+
+	if rq.Get("client_id") == "" {
+		addWarning(CLIENT_ID_WARNING)
+	}
+	if rq.Get("response_type") != "code" {
+		addWarning(RESPONSE_TYPE_WARNING)
+	}
+	if rq.Get("state") == "" {
+		addWarning(STATE_WARNING)
+	}
+
+	if email == "" {
 		queryArgs := make(map[string]string)
 		for k, v := range r.URL.Query() {
 			queryArgs[k] = v[0]
 		}
 		renderLoginPage(w, &loginPageContext{
 			Tagline:   template.HTML(getRandomTagline()),
+			Warnings:  warnings,
 			QueryArgs: queryArgs,
 		})
 	} else {
